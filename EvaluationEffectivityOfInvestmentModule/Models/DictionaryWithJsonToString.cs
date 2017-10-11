acb3854d162062ae39044e5e155be7224834abf8
Герасимюк <d.gerasymiuk@gmail.com>
2017-10-11 22:04:04 +0300

    cdasd

---
+++ EvaluationEffectivityOfInvestmentModule/Models/DictionaryWithJsonToString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace EvaluationEffectivityOfInvestmentModule.Models
+{
+    public class DictionaryWithJsonToString<K,V> : Dictionary<K, V>
+    {
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("");
+            return base.ToString();
+        }
+
+    }
+}
